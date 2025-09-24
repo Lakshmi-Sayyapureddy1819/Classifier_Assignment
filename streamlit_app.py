bed585465eabcd360949fd3365b4e2276bc8491f
Lakshmi Sayyapureddy <142775745+Lakshmi-Sayyapureddy1819@users.noreply.github.com>
2025-09-24 21:49:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,19 @@ import streamlit as st
 import joblib
 import torch
 from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification
+from huggingface_hub import login
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
+# Get Hugging Face token from .env
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+if HF_TOKEN:
+    login(token=HF_TOKEN)
+else:
+    raise ValueError("❌ Hugging Face token not found. Please set HF_TOKEN in .env")
 
 # ——— Load Artifacts ———
 @st.cache_resource
@@ -10,8 +23,8 @@ def load_models():
     vect = joblib.load('model/vectorizer.pkl')
     lr = joblib.load('model/logistic_regression.pkl')
     # Transformer
-    tokenizer = DistilBertTokenizerFast.from_pretrained('model/distilbert')
-    tf_model = DistilBertForSequenceClassification.from_pretrained('model/distilbert')
+    tokenizer = DistilBertTokenizerFast.from_pretrained('NirvanaLohitha/my-classifier-model')
+    tf_model = DistilBertForSequenceClassification.from_pretrained('NirvanaLohitha/my-classifier-model')
     tf_model.eval()
     return vect, lr, tokenizer, tf_model
 
